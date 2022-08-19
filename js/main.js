39e6e4a625249e20b1e49288263740c104e1cd30
qrishunin <qrishunin@gmail.com>
2022-08-19 21:06:37 +0400

    add project bislite

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(function(){
+
+  $('.slider__inner').slick({
+    arrows: false,
+    dots: true,
+  });
+  $('.works__inner').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/arrow-right.svg" alt=""></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/arrow-left.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 911,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 681,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 455,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
+  });
+
+  $('.header__menu-btn').on('click', function(){
+    $('.header__menu > ul').slideToggle();
+  });
+
+  $('.header__drop-down.drop-down').on('click', function () {
+    $(this).children('.drop__down-list').toggleClass('active');
+    $(this).children('.drop-down__link').toggleClass('active');
+  });
+
+  
+});
